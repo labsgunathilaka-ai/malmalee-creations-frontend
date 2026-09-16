ccd1cd1afa5a8286d83bda5734ade97461ed216a
LABS Gunathilaka <labsgunathilaka@gmail.com>
2026-09-16 05:37:13 +0530

    Add Footer component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="font-sans">
-      <Header />
-      <Home />
+    <div className="font-sans min-h-screen flex flex-col justify-between">
+      <div>
+        <Header />
+        <Home />
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
